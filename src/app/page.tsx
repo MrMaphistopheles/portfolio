744cdccfcda1377ae5d7d7b43e89580b399671d5
MrMaphistopheles <basylmarchak@icloud.com>
2024-05-03 16:10:49 +0300

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="w-full px-3">
+      <h1 className=" outline-text lage-text font-bold ">
+        Hi, 👋 i build web application with react and next.js
+      </h1>
+    </div>
+  );
+}
